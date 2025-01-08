ce4dbd9342bfd09c71e24496d4877377f787e395
moamar <168469684+moemuf291@users.noreply.github.com>
2025-01-07 23:04:22 -0500

    Update Apps.py

--- Apps.py
+++ Apps.py
@@ -3,8 +3,7 @@ import json
 import os
 import sys
 
-user_input = input("Weather(1), Currency Coversion(2), Crypto Price(3): ")     # this is where the user will choose what they want and the rest is if elif and else statment depending what the user states as there input
-
+user_input = input("Weather(1), Currency Coversion(2), Crypto Price(3): ")     
 
 if user_input == "1":
     city_name = input("city name:  ")
@@ -12,33 +11,34 @@ if user_input == "1":
         try:
             with open("wea.json", "r") as file:
                 weather = json.load(file)
-                return weather.get("api_key", "error")  # this will get the api from the file called "wea.json"
+                return weather.get("api_key", "error")  
         except FileNotFoundError as e:
             print(e)
-    api_key = weather_api()      # this defines api_key as weather_api() which is what we are going to get from the "wea.json" file
+    api_key = weather_api()     
 
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"   # this takes the user input of the city name and the api key and places this in the spaces for the url
-
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"   
+    
     try:
-        response = requests.get(url, timeout=5)    # this sends the request to the url and has a timeout of 5 secs that means if 5 secs pass and a connection has not been made in will print out a TimeoutError
+        response = requests.get(url, timeout=5)    
+        
     except TimeoutError as e:
         print(e)
     
-    if response.status_code == 200:      # the is saying if the response which is defined as the connection to the server aka "url" was succesfull the we will save the reponse the server sends us as data  the 200 means that connection was succesfull
-        data = response.json()    # defined data as the response from the server we also want it to be in  json format 
+    if response.status_code == 200:      
+        data = response.json()    
         
-        cityname = data["name"]           # this whole this is us access that data and grabbing the stuff we want from it saving it as different variables  
+        cityname = data["name"]             
         temperture = data["main"]["temp"]
         feels = data["main"]["feels_like"]
 
-        temperture_f = 1.8 * (temperture - 273.15) + 32   # both of these are then calculated in fahernet and rounded 
+        temperture_f = 1.8 * (temperture - 273.15) + 32  
         temf_rounded = round(temperture_f)
 
-        feels_f = 1.8 * (feels - 273.15) + 32               # both of these are then calculated in fahernet and rounded 
+        feels_f = 1.8 * (feels - 273.15) + 32              
         feels_round_f = round(feels_f)
 
         print(f"the temperture in {cityname}")
-        print(f"{temf_rounded}°F")                                 # now we print them to the user screen 
+        print(f"{temf_rounded}°F")                                  
         print(f"feels like {feels_round_f}°F")
     else:
         print(ConnectionError)
